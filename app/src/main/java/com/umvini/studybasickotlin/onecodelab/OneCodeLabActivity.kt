5345510a7b1427470951083992257c43939fdd15
Vinicius Alves <viniciusr258@outlook.com>
2022-05-16 12:15:46 -0300

    initial commit

---
+++ app/src/main/java/com/umvini/studybasickotlin/onecodelab/OneCodeLabActivity.kt
@@ -0,0 +1,12 @@
+package com.umvini.studybasickotlin.onecodelab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umvini.studybasickotlin.R
+
+class OneCodeLabActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_one_code_lab)
+    }
+}
