305751798fc09b690888b73d514d48bd7d6956ff
Vinicius Alves <viniciusr258@outlook.com>
2022-05-16 13:45:22 -0300

    Adicionando um padrao a TwoCodeLabActivity e adicionando ItemListActivity

---
+++ app/src/main/java/com/umvini/studybasickotlin/phase/adpter/ItemListActivity.kt
@@ -0,0 +1,8 @@
+package com.umvini.studybasickotlin.phase.adpter
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ItemListActivity (
+    val nameActivity: String,
+    val activity: AppCompatActivity
+)
--- app/src/main/java/com/umvini/studybasickotlin/phase/twocodelab/TwoCodeLabActivity.kt
+++ app/src/main/java/com/umvini/studybasickotlin/phase/twocodelab/TwoCodeLabActivity.kt
@@ -7,13 +7,24 @@ import android.widget.TextView
 import com.umvini.studybasickotlin.R
 
 class TwoCodeLabActivity : AppCompatActivity() {
+
+    lateinit var tvNumber: TextView
+    lateinit var rollButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two_code_lab)
 
-        val tvNumber: TextView = findViewById(R.id.tv_number)
-        val rollButton: Button = findViewById(R.id.bt_roll)
+        initViews()
+        initClicks()
+    }
 
+    private fun initViews() {
+        tvNumber = findViewById(R.id.tv_number)
+        rollButton = findViewById(R.id.bt_roll)
+    }
+
+    private fun initClicks() {
         rollButton.setOnClickListener {
             tvNumber.text = (1..6).random().toString()
         }
